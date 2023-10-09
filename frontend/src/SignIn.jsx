86083fa335f4648410471b2699af9604701e5b72
Jayklef <jerryarhawho@gmail.com>
2023-10-09 01:24:25 +0100

    style.css, onChange, handleSubmit, axios

--- frontend/src/SignIn.jsx
+++ frontend/src/SignIn.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './style.css'
+import axios from 'axios'
 
 function SignIn(){
+
+  const [values, setValues] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('', values)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }
+
   return(
-    <div className='d-flex justify-content-center align-items-center vh-100'>
-        <div className='bg-white p-3 rounded w-25 border'>
+    <div className='d-flex justify-content-center align-items-center vh-100 signinPage'>
+        <div className='bg-white p-3 rounded w-25 border signinForm'>
             <h2>Sign In</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='mb-3'>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" placeholder='Enter email' name='email' className='form-control
+                    <label htmlFor="email"><strong>Email</strong></label>
+                    <input type="email" placeholder='Enter email' name='email' 
+                    onChange={e => e.setValues({...values, email: e.target.value})}     className='form-control
                     rounded-0' />
                 </div>
                 <div className='mb-3'>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" placeholder='Enter password' name='password' className='form-control
+                    <label htmlFor="password"><strong>Password</strong></label>
+                    <input type="password" placeholder='Enter password' name='password' 
+                    onChange={e => e.setValues({...values, password: e.target.value})}    className='form-control
                     rounded-0'/>
                 </div>
                 <button type="submit" className='btn btn-success w-100 rounded-0'>Sign In</button>
                 <p>You agree to our terms and conditions</p>
-                <button className='btn btn-default border w-100 bg-light rounded-0 text-decoration'>
-                  Create Account</button>
             </form>
         </div>
     </div>
